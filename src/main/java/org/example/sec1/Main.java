9f60da8f9f010e0c3b35ab5de0ff1f42e9b4ff61
Mallika-Dey <mallikaju307@gmail.com>
2024-04-20 19:20:57 +0600

    added virtual thread

--- src/main/java/org/example/sec1/Main.java
+++ src/main/java/org/example/sec1/Main.java
@@ -4,11 +4,13 @@ import java.util.concurrent.CountDownLatch;
 
 public class Main {
     private static final int Max_Thread = 10;
+    private static final int Max_Virtual_Thread = 10_000;
 
     public static void main(String[] args) {
         //platFormThread();
 //        platFromThreadBuilder();
-        daemonThread();
+        //daemonThread();
+        virtualThread();
     }
 
     public static void platFormThread() {
@@ -34,6 +36,24 @@ public class Main {
         //platform daemon thread.
         Thread.Builder.OfPlatform builder = Thread.ofPlatform().daemon().name("daemon", 1);
         for (int i = 0; i < Max_Thread; i++) {
+            int j = i;
+            Thread thread = builder.unstarted(() -> {
+                Task.threadRun(j);
+                countDownLatch.countDown();
+            });
+            thread.start();
+        }
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void virtualThread() {
+        CountDownLatch countDownLatch = new CountDownLatch(Max_Virtual_Thread);
+        Thread.Builder builder = Thread.ofVirtual().name("virtual", 1);
+        for (int i = 0; i < Max_Virtual_Thread; i++) {
             int j = i;
             Thread thread = builder.unstarted(() -> {
                 Task.threadRun(j);
