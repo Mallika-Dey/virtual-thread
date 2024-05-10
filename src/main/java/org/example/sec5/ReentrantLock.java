b0d3de8e1a2eb00b6558d9ae89e20b05b71010d9
Mallika-Dey <mallikaju307@gmail.com>
2024-05-10 23:49:26 +0600

    added cpu intensive task reentrantlock

---
+++ src/main/java/org/example/sec5/ReentrantLock.java
@@ -0,0 +1,64 @@
+package org.example.sec5;
+
+import org.example.utils.CommonUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.concurrent.locks.Lock;
+
+public class ReentrantLock {
+    private static final Logger log = LoggerFactory.getLogger(ReentrantLock.class);
+    private static final Lock lock = new java.util.concurrent.locks.ReentrantLock();
+    private static final ArrayList<Integer> list = new ArrayList<>();
+
+    public static void main(String[] args) {
+
+        //Runnable runnable = () -> log.info("new thread");
+        demo(Thread.ofVirtual().name("VT ", 1));
+        //Thread.ofVirtual().start(runnable);
+//            demo(Thread.ofPlatform().name("PT ", 1));
+
+        CommonUtils.sleep(Duration.ofSeconds(150));
+    }
+
+    public static void demo(Thread.Builder thread) {
+        for (int i = 1; i <= 10; i++) {
+            thread.start(() -> {
+                log.info("Thread starting {}", Thread.currentThread());
+                //inMemoryTask();
+                ioTask();
+                //log.info("Thread loop *********** ending {}", Thread.currentThread());
+            });
+        }
+    }
+
+    public static void inMemoryTask() {
+        try {
+            log.info("before locking {}", Thread.currentThread());
+            lock.lock();
+            log.info("virtual thread {} entering", Thread.currentThread());
+            list.add(1);
+            log.info("virtual thread {} ending", Thread.currentThread());
+        } catch (Exception ex) {
+            log.error("error", ex);
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public static void ioTask() {
+        try {
+            log.info("before locking {}", Thread.currentThread());
+            lock.lock();
+            log.info("virtual thread {} entering", Thread.currentThread());
+            CommonUtils.sleep(Duration.ofSeconds(10));
+            log.info("virtual thread {} ending", Thread.currentThread());
+        } catch (Exception ex) {
+            log.error("error", ex);
+        } finally {
+            lock.unlock();
+        }
+    }
+}
